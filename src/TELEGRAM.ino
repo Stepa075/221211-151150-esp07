7f03919a884ebe723e1c58bc9781138e762b00eb
Stacionar-PC <stepa0751@gmail.com>
2022-12-20 17:01:01 +0200

    Worked with one sensor

--- src/TELEGRAM.ino
+++ src/TELEGRAM.ino
@@ -20,11 +20,16 @@ bool waterDetected = false;
 int count = 3;                                                    // Количество сообщений
 const int drop = 250;                                             // Капля
 
+int alarmPin1 = 4; // к выводу 4 подключён геркон 1 
+
 void setup() {
   Serial.begin(115200);
   client.setInsecure();
   
-  pinMode(waterSensor, INPUT_PULLUP);                             // Внутренняя подтяжка сенсора  INPUT_PULLUP
+  pinMode(waterSensor, INPUT_PULLUP);            // Внутренняя подтяжка сенсора  INPUT_PULLUP
+  
+  pinMode(alarmPin1, INPUT_PULLUP); // задаём вывод 4 в качестве входа (будем считывать с него)
+  digitalWrite(alarmPin1, HIGH); // активируем внутренний подтягивающий резистор вывода
 
   // Attempt to connect to Wifi network:
   Serial.print("Соединение с Wifi: ");
@@ -48,20 +53,40 @@ void setup() {
 }
 
 void loop() {
-  int sensorValue = analogRead(waterSensor);                                                // Считываем значение с A0   
-  if(waterDetected && count){
+  
+  int gerkonValue = digitalRead(alarmPin1);                                                // Считываем значение с A0   
+  if(gerkonValue == LOW){
     bot.sendMessage(CHAT_ID, "Сработка!!!", "");
     Serial.println("Сработка!!!");
-    count -= 1;                                                                             // Уменьшаем счётчик на 1  
-    delay(10000);                                                                           // Пауза 10 секунд  
+    // count -= 1;                                                                             // Уменьшаем счётчик на 1  
+    delay(5000);                                                                           // Пауза 5 секунд  
      
   }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // int sensorValue = analogRead(waterSensor);                                                // Считываем значение с A0   
+  // if(waterDetected && count){
+  //   bot.sendMessage(CHAT_ID, "Сработка!!!", "");
+  //   Serial.println("Сработка!!!");
+  //   count -= 1;                                                                             // Уменьшаем счётчик на 1  
+  //   delay(5000);                                                                           // Пауза 5 секунд  
      
-  if (sensorValue > drop) {
-    waterDetected = true;                                                                   // Состояние датчика
-  }
-  else {
-    waterDetected = false;                                                                  // Состояние датчика
-    count = 3;                                                                              // Количество сообщений снова 3
-  }
+  // }
+     
+  // if (sensorValue > drop) {
+  //   waterDetected = true;                                                                   // Состояние датчика
+  // }
+  // else {
+  //   waterDetected = false;                                                                  // Состояние датчика
+  //   count = 3;                                                                              // Количество сообщений снова 3
+  // }
 }
