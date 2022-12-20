216e8f9e1835e427d0e3556468a3bf23c16ae640
Stepa075 <stepa0751@gmail.com>
2022-12-21 00:23:16 +0200

    with for and array

--- src/TELEGRAM.ino
+++ src/TELEGRAM.ino
@@ -3,7 +3,7 @@
 #include <WiFiClientSecure.h>
 #include <UniversalTelegramBot.h>
 #include <ArduinoJson.h>
-
+#include <SoftwareSerial.h>
 // Замените на свои сетевые данные
   const char* ssid = "WIFI_IOT";
   const char* password = "1234567891";
@@ -11,6 +11,8 @@
 // Инициализация Telegram бота
 #define BOTtoken "5906286565:AAF71BxPYkX6sWpgz1wGTdtyKlnffROO3zE"  // Ваш Токен
 #define CHAT_ID "-1001858191181"                                      // ID чата
+
+SoftwareSerial SerialAT(2, 3);
 
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
@@ -20,11 +22,11 @@ UniversalTelegramBot bot(BOTtoken, client);
 // int count = 3;                                                    // Количество сообщений
 // const int drop = 250;                                             // Капля
 
-int alarmPin1 = 4;               // к выводу 4 подключён геркон 1 
-int alarmPin2 = 5;
-int alarmPin3 = 9;
-int alarmPin4 = 10;
-
+int zone1 = 4;               // к выводу 4 подключён геркон 1 
+int zone2 = 5;
+int zone3 = 9;
+int zone4 = 10;
+int arrZones[] = {zone1, zone2, zone3, zone4};
 
 void setup() {
   Serial.begin(115200);
@@ -32,14 +34,14 @@ void setup() {
   
   // pinMode(waterSensor, INPUT_PULLUP);            // Внутренняя подтяжка сенсора  INPUT_PULLUP
   
-  pinMode(alarmPin1, INPUT_PULLUP); // задаём вывод 4 в качестве входа (будем считывать с него)
-  digitalWrite(alarmPin1, HIGH); // активируем внутренний подтягивающий резистор вывода
-  pinMode(alarmPin2, INPUT_PULLUP);
-  digitalWrite(alarmPin2, HIGH);
-  pinMode(alarmPin3, INPUT_PULLUP);
-  digitalWrite(alarmPin3, HIGH);
-  pinMode(alarmPin4, INPUT_PULLUP);
-  digitalWrite(alarmPin4, HIGH);
+  pinMode(zone1, INPUT_PULLUP); // задаём выводы в качестве входа (будем считывать с него)
+  digitalWrite(zone1, HIGH); // активируем внутренний подтягивающий резистор вывода
+  pinMode(zone2, INPUT_PULLUP);
+  digitalWrite(zone2, HIGH);
+  pinMode(zone3, INPUT_PULLUP);
+  digitalWrite(zone3, HIGH);
+  pinMode(zone4, INPUT_PULLUP);
+  digitalWrite(zone4, HIGH);
 
   // Attempt to connect to Wifi network:
   Serial.print("Соединение с Wifi: ");
@@ -63,16 +65,57 @@ void setup() {
   bot.sendMessage(CHAT_ID, "Бот <Охрана> стартовал", "");
 }
 
+
+
+
 void loop() {
   
-  int gerkonValue = digitalRead(alarmPin1);                                                // Считываем значение с A0   
-  if(gerkonValue == LOW){
-    bot.sendMessage(CHAT_ID, "Сработка!!!", "");
-    Serial.println("Сработка!!!");
+  for (int i = 0; i<sizeof(arrZones)/sizeof(int); i++){
+    int gerkonValue1 = digitalRead(arrZones[i]);                                                // Считываем значение с A0   
+  if(gerkonValue1 == LOW){
+    bot.sendMessage(CHAT_ID, "Сработка зона1!!!", "");
+    Serial.println("Сработка зона1!!!" + arrZones[i]);
     // count -= 1;                                                                             // Уменьшаем счётчик на 1  
-    delay(5000);                                                                           // Пауза 5 секунд  
-     
+    delay(1000);                                                                           // Пауза 5 секунд  
   }
+  }
+
+
+  // int gerkonValue1 = digitalRead(zone1);                                                // Считываем значение с A0   
+  // if(gerkonValue1 == LOW){
+  //   bot.sendMessage(CHAT_ID, "Сработка зона1!!!", "");
+  //   Serial.println("Сработка зона1!!!");
+  //   // count -= 1;                                                                             // Уменьшаем счётчик на 1  
+  //   delay(1000);                                                                           // Пауза 5 секунд  
+  // }
+  //  int gerkonValue2 = digitalRead(zone2);                                                // Считываем значение с A0   
+  // if(gerkonValue2 == LOW){
+  //   bot.sendMessage(CHAT_ID, "Сработка зона2!!!", "");
+  //   Serial.println("Сработка зона2!!!");
+  //   // count -= 1;                                                                             // Уменьшаем счётчик на 1  
+  //   delay(1000);                                                                           // Пауза 5 секунд  
+  // }
+  //  int gerkonValue3 = digitalRead(zone3);                                                // Считываем значение с A0   
+  // if(gerkonValue3 == LOW){
+  //   bot.sendMessage(CHAT_ID, "Сработка зона3!!!", "");
+  //   Serial.println("Сработка зона3!!!");
+  //   // count -= 1;                                                                             // Уменьшаем счётчик на 1  
+  //   delay(1000);                                                                           // Пауза 5 секунд  
+  // }
+  //  int gerkonValue4 = digitalRead(zone4);                                                // Считываем значение с A0   
+  // if(gerkonValue4 == LOW){
+  //   bot.sendMessage(CHAT_ID, "Сработка зона4!!!", "");
+  //   Serial.println("Сработка зона4!!!");
+  //   // count -= 1;                                                                             // Уменьшаем счётчик на 1  
+  //   delay(1000);                                                                           // Пауза 5 секунд  
+  // }
+
+
+
+
+}
+
+  
   
   
   
@@ -100,4 +143,4 @@ void loop() {
   //   waterDetected = false;                                                                  // Состояние датчика
   //   count = 3;                                                                              // Количество сообщений снова 3
   // }
-}
+
